f20380ded3448291233bd33c63be0ec2bda13aa3
danpraset <danpraset@gmail.com>
2020-10-08 13:06:27 -0700

    Homework Problem 2 done w/ comments

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,6 +2,25 @@ public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
-        // Add your solution
+        int x = data.length;
+
+        for (int i = 0; i <= x - 1; i++)
+        {
+            // Second loop goes through array
+            for (int j = 0; j < x - i - 1; j++)
+            {
+                if (data[j] > data[j + 1]) {
+                    // Swap j with j+1
+                    // Set int temp to equal j
+                    int temp = data[j];
+
+                    // Sets data[j] to equal j+1
+                    data[j] = data[j + 1];
+
+                    // Sets j+1 to equal temp, swapping them
+                    data[j + 1] = temp;
+                }
+            }
+        }
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,31 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        int [][] inputs =
+                {
+                        {},
+                        {3},
+                        {3, 1},
+                        {3, 5, 9},
+                        {13, 4, 9, 26, 1},
+                        {5, 3, 11, 21},
+                        {1, 12, 6, 17},
+                };
+
+        int [][] expected =
+                {
+                        {},
+                        {3},
+                        {1, 3},
+                        {3, 5, 9},
+                        {1, 4, 9, 13, 26},
+                        {3, 5, 11, 21},
+                        {1, 6, 12, 17}
+                };
+        for (int i = 0; i < inputs.length; i++)
+        {
+            Problem2.bubbleSort(inputs[i]);
+            assertArrayEquals(inputs[i], expected[i]);
+        }
     }
 }
